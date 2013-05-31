15ddd3d0aa21cec9ae959c8b3d4f7c15068d6c98
Neil Middleton <neil@heroku.com>
2013-05-31 13:40:07 +0100

    Move to Rspec
    Minitest hurts

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'attr_secure'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  config.order = 'random'
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
