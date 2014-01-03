01e63156ed438109aabe65d1b5dd525bf711281b
Maciek Sakrejda <maciek@heroku.com>
2014-01-02 17:18:10 -0800

    Test Secure#secret setter for multiple-secret case

--- spec/secure_spec.rb
+++ spec/secure_spec.rb
@@ -33,8 +33,17 @@ describe AttrSecure::Secure do
   end
 
   context "with an array of keys" do
-    subject      { described_class.new(secret) }
-    let(:secret) { 'fWSvpC6Eh1/FFE1TUgXpcEzMmmGc9IZSqoexzEslzKI=,d9ssNmUYn7UpMoSc0eM2glVUG2DPYwXveLTDU7j8pBY=' }
+    subject       { described_class.new([secret1, secret2].join(',')) }
+    let(:secret1) { 'fWSvpC6Eh1/FFE1TUgXpcEzMmmGc9IZSqoexzEslzKI=' }
+    let(:secret2) { 'd9ssNmUYn7UpMoSc0eM2glVUG2DPYwXveLTDU7j8pBY=' }
+
+    describe '#secret=' do
+      it "should update the list of secrets" do
+        expect(subject.secret).to eq([secret1, secret2])
+        subject.secret = [ secret2, secret1 ].join(',')
+        expect(subject.secret).to eq([secret2, secret1])
+      end
+    end
 
     describe '#encrypt' do
       it "should encrypt a string" do
